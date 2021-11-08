821f27b3a6d9e3936998fa65a2e8a451ac91ac10
Nalin Kularathna <pgnaleen@gmail.com>
2021-11-08 12:10:33 +0530

    [GSIS-29] - navigation bar to support dynamic permission

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,24 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
-declare const $: any;
 declare interface RouteInfo {
-    path: string;
-    title: string;
-    icon: string;
-    class: string;
+  key: string;
+  path: string;
+  title: string;
+  icon: string;
+  class: string;
+  children?: RouteInfo[];
 }
 
+const allowMenuItems = ['admin', 'test', 'test.comp1', 'test.comp2', 'test.comp2.comp4'];
 
- const allowssidemenuitems = ['admin', 'dashoboard'];
-
-// admin.add.info
-// admin.add.user
 export const ROUTES: RouteInfo[] = [
-  // {}
-    // { key: 'admin.add.user', path: '/useradd', title: 'Dashboard',  icon: 'dashboard', class: '' },
-    // { key: 'admin.add.info', path: '/infoadd', title: 'Dashboard',  icon: 'dashboard', class: '' },
-    // { key: 'admin.add', path: '/add', title: 'Dashboard',  icon: 'dashboard', class: '' },
-    { path: '/admin', title: 'Admin',  icon: 'assignment_ind', class: '' }
+
+  { key: 'infoadd', path: '/infoadd', title: 'Dashboard',  icon: 'dashboard', class: '' },
+  { key: 'add', path: '/add', title: 'Dashboard',  icon: 'dashboard', class: '' },
+  { key: 'admin', path: '/admin', title: 'Admin', icon: 'assignment_ind', class: ''}
+  , { key: 'test', path: '/test', title: 'Test children', icon: 'notifications', class: '', children: [
+      {key: 'test.comp1', path: '/test-comp1', title: 'Component 1', icon: 'dashboard', class: ''},
+      {key: 'test.comp2', path: '/test-comp2', title: 'Component 2', icon: 'person', class: '', children: [
+          {key: 'test.comp2.comp3', path: '/test-comp3', title: 'Component 3', icon: 'dashboard', class: ''},
+          {key: 'test.comp2.comp4', path: '/test-comp4', title: 'Component 4', icon: 'person', class: ''},
+        ]},
+    ]
+  },
 ];
 
 @Component({
@@ -29,15 +34,44 @@ export const ROUTES: RouteInfo[] = [
 export class SidebarComponent implements OnInit {
   menuItems: any[];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
-    this.menuItems = ROUTES.filter(menuItem => menuItem);// forloop
-  }
-  isMobileMenu() {
-      if ($(window).width() > 991) {
-          return false;
+    this.menuItems = ROUTES.filter(menuItem => {
+      if (allowMenuItems.includes(menuItem.key)) {// check whether main menu authorized, include in the list
+        if (menuItem.children){// if there are children then check there permission seperately
+          return this.removeUnauthorizedSubMenuItems(menuItem);
+        } else {
+          return menuItem;
+        }
       }
-      return true;
+
+    });// forloop
+  }
+
+  removeUnauthorizedSubMenuItems(menuItem: RouteInfo): RouteInfo {
+    console.log(menuItem + 'menu item has children')
+
+    menuItem.children = menuItem.children.filter(submenuItem => {
+      if (allowMenuItems.includes(submenuItem.key)) {
+        // if submenu also has submenus and so on
+        if (submenuItem.children){// if there are children for first level submenu and so on, then check there permission seperately
+          return this.removeUnauthorizedSubMenuItems(submenuItem);
+        } else {
+          return submenuItem;
+        }
+      } else { // no else path as this filter copy one list to another. as submenuitem not in the list no need to copy
+      }
+    });
+
+    return menuItem;
+  }
+
+  isMobileMenu() {
+    if ($(window).width() > 991) {
+      return false;
+    }
+    return true;
   };
 }
