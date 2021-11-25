c1e9a141e2f205e3227ca10fefca2f5e9cf6fc6a
pgnaleen <pgnaleen@gmail.com>
2021-11-25 19:32:01 +0530

    [GSIS-67] - setting dev server back end

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: true,
   authUrl: 'http://localhost:8080/auth',
   // authUrl: 'http://10.0.0.11:8082/auth',
-  adminAPIServer: 'http://localhost:8089',
-  // adminAPIServer: 'http://10.0.0.11:8089',
+  // adminAPIServer: 'http://localhost:8089',
+  adminAPIServer: 'http://10.0.0.11:8089',
   // redirectUrl: 'http://localhost:4200',
   redirectUrl: 'http://10.0.0.11:4200',
   keyCloakRealm: 'gutech'
