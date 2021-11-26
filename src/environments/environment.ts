6487d2c0b15c1e2db93d6592655d418ab9543c3c
pgnaleen <pgnaleen@gmail.com>
2021-11-26 11:13:54 +0530

    [GSIS-67] - back end api server port change

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
   authUrl: 'http://localhost:8080/auth',
   // authUrl: 'http://10.0.0.11:8082/auth',
   // adminAPIServer: 'http://localhost:8089',
-  adminAPIServer: 'http://10.0.0.11:8089',
+  adminAPIServer: 'http://10.0.0.11:8284',
   // redirectUrl: 'http://localhost:4200',
   redirectUrl: 'http://10.0.0.11:4200',
   keyCloakRealm: 'gutech'
